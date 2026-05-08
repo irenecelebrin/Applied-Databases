751a408aea7ae20ca4648bd74b1f6e7890830f23
Irene Celebrin <irene.celebrin@gmail.com>
2026-05-08 19:52:40 +0100

    add comment to main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# main file to run the application. To run the application, execute the following command: python main.py
+
+# import menu 
 from menu import main
 
+# -------------------------------------------
 if __name__ == '__main__':
     main()
